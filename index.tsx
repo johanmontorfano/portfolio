62ce81ce017122f30f2c9791d14a8e626c82724e
Johan Montorfano <me@johanmontorfano.com>
2024-05-17 18:28:46 +0200

    did necessary refactorings and changed the style of the whole websitge

--- index.tsx
+++ index.tsx
@@ -1,72 +1,27 @@
-import "./src/three";
-import {render} from "solid-js/web";
-import { Bouncer } from "./src/components/bounce";
-import { createSignal } from "solid-js";
-import { Motion } from "solid-motionone";
-import { Welcome } from "./src/components/welcome";
-import { Projects } from "./src/components/projects";
+import { render } from "solid-js/web";
+import { renderer as ThreeScene } from "./src/three";
 
-function Index() {
-    const [began, setBegin] = createSignal(false);
-    const [showProjects, setShowProjects] = createSignal(false);
-    /** We keep a reference to the three parent to perform updates depending on
-     * the progression of the user on the page. */
-    const three_ref = document.getElementById("three_main") as HTMLElement;
-
+function App() {
     return <div style={{
-        width: "100vw", 
+        width: "100%",
         height: "100dvh",
-        background: began() ? "var(--t-background)" : "rgba(0, 0, 0, 0)",
-        "transition-duration": "0.5s"
+        background: "var(--background)",
     }}>
-        <Motion.div 
-            style={{
-                position: "absolute", 
-                left: "50%", 
-                top: "80%",
-                transform: "translateX(-50%)"
-            }}
-            animate={{
-                opacity: began() ? 0 : 1,
-                visibility: began() ? "hidden": "visible"
-            }}
-        >
-            <Bouncer onClick={() => {
-                 three_ref.style.setProperty("transition-duration", "0.5s");
-                 three_ref.style.setProperty("transform", "scale(0.5)");
-                 setBegin(true);
-            }} />
-        </Motion.div>
-        <Motion.div
-            style={{
-                position: "absolute",
-                width: "100%",
-                height: "100dvh",
-                display: "flex",
-                "align-items": "center",
-                "justify-content": "center",
-                "overflow-x": "hidden"
-            }}
-            initial={{
-                visibility: "hidden"
-            }}
-            animate={{
-                visibility: began() ? "visible" : "hidden",
-            }}
-            transition={{
-                visibility: {duration: 0}
-            }}
-        >
-            <Welcome
-                projectsOpen={showProjects()}
-                setProjectsOpen={setShowProjects} 
-            />
-            <Projects 
-                show={showProjects} 
-                onRequestHide={() => setShowProjects(false)} 
-            />
-        </Motion.div>
+        <div style={{
+            position: "absolute", 
+            bottom: "0%",
+            padding: "2%"
+        }}>
+            <div>
+                {ThreeScene.domElement} 
+            </div>
+            <div>
+                <a href="https://linkedin.com/in/jhnm">LinkedIn</a>
+                <a href="https://github.com/johanmontorfano">Github</a>
+                <a href="mailto:me@johanmontorfano.com">Contact</a>
+            </div>
+        </div>
     </div>
 }
 
-render(Index, document.getElementById("solid_main") as HTMLElement);
+render(App, document.getElementById("main") as HTMLElement);
