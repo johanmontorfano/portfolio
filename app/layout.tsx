4845a549a03646a8dec2511abba3920242375425
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-02-13 12:24:02 +0100

    add: page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
 import "@/app/index.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Johan Delhomme Montorfano's Portfolio",
+}
 
 export default function RootLayout(props: { children: ReactNode }) {
     return <html lang="en">
