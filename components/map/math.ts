12b910c84ca530bd34600c45441699844ef07856
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-04-20 02:42:49 +0200

    add: 2D distance formula, hex color mixing

--- components/map/math.ts
+++ components/map/math.ts
@@ -21,6 +21,10 @@ export const relativeDistance = (r: XY, a: XY, b: XY) => {
     return da - db;
 }
 
+export const distance = (a: XY, b: XY) => {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
 // selects a X random unique numbers of a number set size
 export const rsizeget = (target: number, setSize: number) => {
     if (target <= 0) return [];
@@ -30,4 +34,31 @@ export const rsizeget = (target: number, setSize: number) => {
 
     while (out.size < target) out.add(randint(0, setSize));
     return Array.from(out);
+}
+
+// mix (100 - X)% of a with X% of b
+// both colors should be hex-formatted colors
+export const hexmix = (a: string, b: string, mix: number): string => {
+    mix = Math.min(Math.max(0, mix), 1)
+
+    const parse = (hex: string) => {
+        const clean = hex.replace('#', '');
+        return {
+            r: parseInt(clean.substring(0, 2), 16),
+            g: parseInt(clean.substring(2, 4), 16),
+            b: parseInt(clean.substring(4, 6), 16),
+        };
+    };
+    const encode = (c: number) => 
+        Math.round(c).toString(16).padStart(2, '0');
+
+    const rgbA = parse(a);
+    const rgbB = parse(b);
+    const mixed = {
+        r: rgbA.r * (1 - mix) + rgbB.r * mix,
+        g: rgbA.g * (1 - mix) + rgbB.g * mix,
+        b: rgbA.b * (1 - mix) + rgbB.b * mix,
+    };
+
+    return `#${encode(mixed.r)}${encode(mixed.g)}${encode(mixed.b)}`;
 }
