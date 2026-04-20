6a44488d6fca58d73fb0d5ec4dc23e8b1e8d5eaa
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-04-20 02:41:28 +0200

    add: dotted map controller

---
+++ hooks/use_dotted_map.ts
@@ -0,0 +1,30 @@
+import { Link } from "@/components/map/links";
+import { RC } from "@/components/map/types";
+
+type CreateNode = { rc: RC };
+type CreateLink = { link: Link };
+type RemoveLink = { rmlink: Link };
+type ChangeColor = { accent: string };
+type ForceRenderLayer = { name: "backdrop" };
+
+type Commands = CreateNode |
+    CreateLink |
+    RemoveLink |
+    ChangeColor |
+    ForceRenderLayer;
+
+const commands: Commands[] = [];
+
+// The dotted map controller is used to trigger events on the dotted map
+// without having to interfere with the map data and props. it acts as A FIFO
+// queue the dotted map goes through at each render
+export function useDottedMapController() {
+    return {
+        trigger(cmd: Commands) {
+            commands.push(cmd);
+        },
+        readAll(fn: (cmd: Commands) => void) {
+            commands.splice(0).forEach(fn);
+        }
+    }
+}
