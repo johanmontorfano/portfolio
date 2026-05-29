cd5897ee6cd7c9acba9203fd3e47d4d79e5054b2
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-05-29 15:15:53 +0200

    add: extended markdown support

--- components/mini_md.tsx
+++ components/mini_md.tsx
@@ -1,10 +1,11 @@
 import MD from "react-markdown";
 import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 
 import "@/app/md.css";
 
 export function Markdown(props: { content: string }) {
     return <div className="md-container">
-        <MD rehypePlugins={[rehypeSlug]}>{props.content}</MD>
+        <MD rehypePlugins={[rehypeSlug, remarkGfm]}>{props.content}</MD>
     </div>
 }
