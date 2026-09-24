e63915a7393509f5428c29c62b67828bb8b7ab20
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-09-24 03:02:36 +0200

    fix: margin

--- app/(protected)/apps/layout.tsx
+++ app/(protected)/apps/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/admin_navbar";
 import { ReactNode } from "react";
 
 export default async function Layout(props: { children: ReactNode }) {
-    return <div className="w-[90%] max-w-[1000px] mx-auto">
+    return <div className="w-[95%] max-w-[1000px] mx-auto">
         <Navbar />
         {props.children}
     </div>
