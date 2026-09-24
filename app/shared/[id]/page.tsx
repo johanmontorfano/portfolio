cba6ccb9a45c5ff1591be55ef1845cdc0ead798d
Johan Delhomme Montorfano <me@johanmontorfano.com>
2026-09-24 02:20:29 +0200

    fix: download page is now more responsive and has a better UI

--- app/shared/[id]/page.tsx
+++ app/shared/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { retrieveFileByRef } from "@/scripts/fb_utils/file_mgr";
 import { Metadata } from "next";
 import { BsDownload } from "react-icons/bs";
-import Logo from "@/public/assets/logo.svg";
-import Image from "next/image";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
     title: "Shared file – JWS Drive",
@@ -11,38 +10,42 @@ export const metadata: Metadata = {
     }
 }
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+async function Dialog(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const fileData = await retrieveFileByRef(params.id);
 
     if (!fileData)
         return <p>File not found :(</p>;
 
+    return <>
+        <div className="flex justify-between">
+            <h1 className="card-title">
+                {fileData.filename}
+            </h1>
+            <a
+                className="btn hover:btn-primary btn-outline btn-sm"
+                href={fileData.downloadUrl}
+                target="_blank"
+            >
+                <BsDownload /> Download
+            </a>
+        </div>
+        <p className="opacity-80">{fileData.description}</p>
+        <p className="opacity-60">
+            You can download this file now or come back later.
+        </p>
+    </>
+}
+
+export default function Page(props: { params: Promise<{ id: string }> }) {
     return <div className="w-full h-dvh flex justify-center items-center">
-        <div className="card max-w-[400px]!">
-            <figure>
-                <Image alt="logo" src={Logo} className="w-16 h-16" />
-            </figure>
-            <div className="card-body">
-                <h1 className="card-title text-2xl">
-                    You have been shared {fileData.filename}
-                </h1>
-                <div className="bg-base-300 italic p-4 rounded-lg">
-                    <p>{fileData.description}</p>
-                </div>
-                <p className="opacity-60 mt-2">
-                    You can download this file now or come back later.
-                </p>
-                <div className="flex justify-end mt-2">
-                    <a
-                        className="btn btn-primary"
-                        href={fileData.downloadUrl}
-                        target="_blank"
-                    >
-                        <BsDownload />
-                        Download
-                    </a>
-                </div>
+        <div className="card max-w-[700px]">
+            <div className="card-body space-y-4">
+                <Suspense fallback={
+                    <span className="loading loading-spinner" />
+                }>
+                    <Dialog params={props.params} />
+                </Suspense>
             </div>
         </div>
     </div>
